e8e1d5702cfd15d7384b128faf94e5d87de2cc65
Mitskevich Victor <mv2.mx@yandex.ru>
2019-10-15 10:37:00 +0300

    feat: add node solution

--- src/node.js
+++ src/node.js
@@ -1,22 +1,48 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (node === null) {
+			return;
+		} else if (this.left === null) {
+			this.left = node;
+			this.left.parent = this;
+		} else if (this.right === null) {
+			this.right = node;
+			this.right.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (node === null) {
+			return;
+		} else if (this.left === node) {
+			this.left.parent = null;
+			this.left = null;
+		} else if (this.right === node) {
+			this.right.parent = null;
+			this.right = null;
+		} else throw "Error";
 	}
 
 	remove() {
-
+		if (this.parent === null) {
+			return;
+		} else {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.parent === null) {
+			return;
+		}
 	}
 }
 
